d23162dd93afe3cca419b91d8622790642062e61
MDietermann <mdietermann253@gmail.com>
2025-04-27 22:02:56 +0200

    Add ExampleProject

---
+++ ExampleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using TextRpgLib;
+using TextRpgLib.content_modules.item_module.core;
+
+namespace LibTester;
+
+class Program {
+    static void Main(string[] args) {
+        string yamlFolderLocation = Path.GetFullPath("../../../Data/");
+
+        Init init = new Init(yamlFolderLocation);
+        Dictionary<string, List<Item>>? usables = init.TryGetValue("usables") as Dictionary<string, List<Item>>;
+        Dictionary<string, List<Item>>? currencies = init.TryGetValue("currencies") as Dictionary<string, List<Item>>;
+        List<Item> potions = usables["Potions"];
+        List<Item> humanCurrency = currencies["HumanCurrency"];
+        Console.WriteLine("Loading Complete!");
+    }
+}
